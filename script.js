66a293cd903631fffe06ea225a4a293b29aaa38c
Ali Abid <alabid@google.com>
2018-11-09 19:08:57 -0800

    more

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-$(".answers button").click(function (evt) {
+var answers;
+$.get("https://raw.githubusercontent.com/aliabid94/gmat_review/master/answers.yaml", function(data) {
+  answers = YAML.parse(data)
+  var question_numbers = getRandomSample(Object.keys(answers).length-1, 10)
+  var html = ""
+  for (var i = 0; i < question_numbers.length; i++) {
+    question_number = question_numbers[i]+1
+    answer = answers[question_number]
+    html += `
+      <div class='problem'>
+        <div class='ui message'>
+          <div class='question-number'>
+            <div class='ui header'>Question ${i+1}</div>
+          </div>
+          <div>
+            <div class='question ui segment'>
+              <img src='questions/${question_number}.png'>
+            </div>
+            <div class='answers'>
+              <div class='ui vertical buttons'>
+                <button class='ui button fluid'>A</button>
+                <button class='ui button fluid'>B</button>
+                <button class='ui button fluid'>C</button>
+                <button class='ui button fluid'>D</button>
+                <button class='ui button fluid'>E</button>
+              </div>
+            </div>
+          </div>
+          ${answer["hints"].map(str => "<div class='hint'>"+katex.renderToString(str)+"</div>").join("")}
+          <button class='ui yellow button add_hint'>Hint?</button>
+        </div>
+      </div>
+    `
+  }
+  $("body").append(html);
+});
+
+$("body").on("click", ".answers button", function (evt) {
   $(evt.target).parent().find("button").removeClass("blue")
   $(evt.target).addClass("blue")
 });
 
-$(".add_hint").click(function (evt) {
+$("body").on("click", ".add_hint", function (evt) {
   var hints = $(evt.target).parent().find(".hint")
   var opened = false;
   var hints_complete = true
@@ -23,9 +60,4 @@ $(".add_hint").click(function (evt) {
   } else {
     $(evt.target).text("Another Hint?")
   }
-});
-    
-$.get("https://raw.githubusercontent.com/aliabid94/gmat_review/master/answers.yaml", function(data) {
-  var answers = YAML.parse(data)
-  console.log(answers)
 });
---
+++ util.js
@@ -0,0 +1,12 @@
+function getRandomSample(range, size) {
+    var arr = []
+    for (var j = 0; j <= range; arr.push(j++)) {}
+    var shuffled = arr.slice(0), i = arr.length, min = i - size, temp, index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
+}
