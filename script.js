ef7612be1d53ae180eff9aa88fc536f806646b9a
Ali Abid <alabid@google.com>
2018-11-09 17:26:28 -0800

    more prog

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ $(".answers button").click(function (evt) {
   $(evt.target).addClass("blue")
 });
 
-$(".hint").click(function (evt) {
-  $(evt.target).text("Another Hint?")
+$(".add_hint").click(function (evt) {
+  var hints = $(evt.target).parent().find(".hint")
+  var opened = false;
+  var hints_complete = true
+  for (var i = 0; i < hints.length; i++) {
+    if (opened) {
+      hints_complete = false
+    }
+    var hint = hints[i];
+    if ($(hint).is(":hidden") && opened == false) {
+      $(hint).show();
+      opened = true;
+    }
+  }
+  
+  if (hints_complete) {
+    $(evt.target).hide()
+  } else {
+    $(evt.target).text("Another Hint?")
+  }
+});
+    
+$.get("https://raw.githubusercontent.com/aliabid94/gmat_review/master/answers.yaml", function(data) {
+  var answers = YAML.parse(data)
+  console.log(answers)
 });
