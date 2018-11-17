960f92282cd2864f1736365441ddb601385aa8a6
Ali Abid <alabid@google.com>
2018-11-16 19:56:13 -0800

    push

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function render_to_string(i, question_number, answer, correct_answer) {
+function render_to_string(i, question_number, answer) {
   return `
    <div class='ui message'>
     <div class='question-number'>
@@ -10,15 +10,15 @@ function render_to_string(i, question_number, answer, correct_answer) {
       </div>
       <div class='answers'>
         <div class='ui vertical buttons'>
-          <button class='ui button fluid choice ${correct_answer == "A" ? "correct" : ""}'>${answer.type == "data_sufficiency" ? "Statement 1 only" : "A"}</button>
-          <button class='ui button fluid choice ${correct_answer == "B" ? "correct" : ""}'>${answer.type == "data_sufficiency" ? "Statement 2 only" : "B"}</button>
-          <button class='ui button fluid choice ${correct_answer == "C" ? "correct" : ""}'>${answer.type == "data_sufficiency" ? "Either Statement 1 or 2" : "C"}</button>
-          <button class='ui button fluid choice ${correct_answer == "D" ? "correct" : ""}'>${answer.type == "data_sufficiency" ? "Statement 1 & 2 together" : "D"}</button>
-          <button class='ui button fluid choice ${correct_answer == "E" ? "correct" : ""}'>${answer.type == "data_sufficiency" ? "Together insufficient" : "E"}</button>
+          <button class='ui button fluid choice ${answer["answer"] == "A" ? "correct" : ""}'>${answer.type == "data_sufficiency" ? "Statement 1 only" : "A"}</button>
+          <button class='ui button fluid choice ${answer["answer"] == "B" ? "correct" : ""}'>${answer.type == "data_sufficiency" ? "Statement 2 only" : "B"}</button>
+          <button class='ui button fluid choice ${answer["answer"] == "C" ? "correct" : ""}'>${answer.type == "data_sufficiency" ? "Either Statement 1 or 2" : "C"}</button>
+          <button class='ui button fluid choice ${answer["answer"] == "D" ? "correct" : ""}'>${answer.type == "data_sufficiency" ? "Statement 1 & 2 together" : "D"}</button>
+          <button class='ui button fluid choice ${answer["answer"] == "E" ? "correct" : ""}'>${answer.type == "data_sufficiency" ? "Together insufficient" : "E"}</button>
         </div>
       </div>
     </div>
-    ${answer["hints"].map(str => "<div class='hint'>"+katex.renderToString(str)+"</div>").join("")}
+    ${(answer["hints"] || []).map(str => "<div class='hint'>"+katex.renderToString(str)+"</div>").join("")}
     <button class='ui blue button add_hint'>Hint?</button>
   </div>`
 }
